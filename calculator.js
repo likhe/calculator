57eb936f0b761e9c548beb9755cfd11b796393cf
likhe <likhelifi@laposte.net>
2016-11-29 18:15:54 +0100

    try to understand

---
+++ calculator.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const ready = function() {
+  document.querySelector("form").addEventListener("submit", function(e))
+}
+
+
+/*Quand l'utilisateur clique sur le bouton type submit
+ document.addEventListener("DOMContentLoaded", function() {
+            document.querySelector("math").addEventListener("click", function() {
+              //
+                let val1 = parseInt(document.getElementById('num1').value);
+                let val2 = parseInt (document.getElementById('num2').value);
+                let result = val1 + val2;
+                console.log(result);
+                document.getElementById('result').innerHTML = result;
+            })
+        });
+*/
